24c8db47a99f698e8e63c6883c7e78af52069184
miullu <satou.ide@gmail.com>
2026-01-31 11:20:50 +0800

    formal package name

--- src/EpubParser.kt
+++ src/EpubParser.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.sample.app
+package cmoon
 
 import android.content.Context
 import android.graphics.Bitmap
--- src/MainActivity.kt
+++ src/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.sample.app
+package cmoon
 
 import android.net.Uri
 import android.os.Bundle
